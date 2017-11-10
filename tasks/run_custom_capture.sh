c6533bf9caaa1d42fc474111c7bfe9edbb7a1a14
Stefan <stefan@puppet.com>
2017-11-10 12:47:30 +0000

    Logic fix on custom capture task

--- tasks/run_custom_capture.sh
+++ tasks/run_custom_capture.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 # Puppet Task Name: run_custom_capture
+interface=""
+savefile=""
 if [[ -z $PT_packetcount ]]
   then PT_packetcount="-c 50"
 elif [[ $PT_packetcount == "none" ]]
   then PT_packetcount=""
 fi
-
-interface="-i "$PT_interface
-savefile="-w "$PT_savefile
+if [[ -z $PT_interface  ]]
+  then interface="-i "$PT_interface
+fi
+if [[ -z $PT_savefile  ]]
+  then savefile="-w "$PT_savefile
+fi
 
 $(/usr/bin/which tcpdump) $interface $PT_packetcount $savefile $PT_capturefilter
