56f545fcfdd98948ba8b69daedeb749e5305ce75
Stefan <stefan@puppet.com>
2017-11-10 09:59:46 +0000

    Added better packet count failsafe logic to custom capture task

--- tasks/run_custom_capture.sh
+++ tasks/run_custom_capture.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-# Puppet Task Name: run_custom_apture
+# Puppet Task Name: run_custom_capture
+if [[ -z $PT_packetcount ]]
+ then PT_packetcount="-c 50"
+fi
 $(/usr/bin/which tcpdump) $PT_interface $PT_packetcount $PT_savefile $PT_capturefilter
